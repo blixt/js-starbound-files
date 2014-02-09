bed311d56deef263dc6b53a109ac5cd1fe0d0c5d
Blixt <blixt@spotify.com>
2014-02-09 14:57:02 -0500

    Add module for reading .pak files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   blockfile: require('./lib/blockfile'),
   btreedb: require('./lib/btreedb'),
+  package: require('./lib/package'),
   sbon: require('./lib/sbon'),
   world: require('./lib/world')
 };
