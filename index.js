66a4380f61cb168bf54e14a06d178238f803aca4
Blixt <blixt@spotify.com>
2014-03-08 15:39:46 -0500

    Add support for SBVJ01 files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.BlockFile = require('./lib/blockfile');
 exports.BTreeDB = require('./lib/btreedb');
+exports.Document = require('./lib/document');
 exports.Package = require('./lib/package');
 exports.SbonReader = require('./lib/sbonreader');
 exports.World = require('./lib/world');
